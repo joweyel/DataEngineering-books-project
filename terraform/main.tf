f03497096e8626eb34f8b578f4fd84cd0011a68c
joweyel <falsh@protonmail.com>
2025-01-07 19:46:19 +0100

    AWS_REGION variable added

--- terraform/main.tf
+++ terraform/main.tf
@@ -278,5 +278,6 @@ data "template_file" "docker_compose" {
     POSTGRES_HOST     = "localhost" # aws_rds_cluster.de-aurora-cluster.endpoint
     POSTGRES_PORT     = 5432        # aws_rds_cluster.de-aurora-cluster.port
     S3_BUCKET_NAME    = var.s3-bucket-name
+    AWS_REGION        = var.region
   }
 }
