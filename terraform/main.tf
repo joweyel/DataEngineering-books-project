fa8f2866f108828e7bc2b3c46b8944c413a574d5
joweyel <falsh@protonmail.com>
2025-01-07 17:17:22 +0100

    added missing file, fixed typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -291,6 +291,6 @@ resource "aws_secretsmanager_secret_version" "access-keys-version" {
   secret_id = aws_secretsmanager_secret.access-keys.id
   secret_string = jsonencode({
     AWS_ACCESS_KEY_ID = var.aws-access-key-id,
-    AWS_SECRET_ACCESS_KEY = aws-secret-access-key
+    AWS_SECRET_ACCESS_KEY = var.aws-secret-access-key
   })
 }
