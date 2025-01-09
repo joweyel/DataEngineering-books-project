bca3c0bc1f3c805c7de44841fe120faa606ff98d
joweyel <falsh@protonmail.com>
2025-01-09 19:52:57 +0000

    removed unused code

--- mage_books/data_loaders/read_data.py
+++ mage_books/data_loaders/read_data.py
@@ -27,21 +27,12 @@ def load_data(*args, **kwargs) -> Tuple[
     data_names = ["Books.csv", "Users.csv", "Ratings.csv"]
     save_path = "/home/src/data"
 
-    # Check if local data used
-    local = kwargs.get("local", "False")
-    if isinstance(local, str):
-        load_local = local.lower() == "true"
-    else:
-        load_local = bool(local)
+    print("Load data from Kaggle")
+    api = KaggleApi()
+    api.authenticate()
+    api.datasets_list()
+    api.dataset_download_files(dataset=ds_name, path=save_path, force=False, unzip=True)
 
-    if load_local == False:
-        print("Load data from Kaggle")
-        api = KaggleApi()
-        api.authenticate()
-        api.datasets_list()
-        api.dataset_download_files(dataset=ds_name, path=save_path, force=False, unzip=True)
-    else:
-        print("Load data locally")
 
     data = {
         "books": pd.read_csv(f"{save_path}/Books.csv"),
